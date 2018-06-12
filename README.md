0fd017fe7bd0596e2d49797c52c5a97e757c48ef
Mr. GoodMan <mrgoodman@trying.tribute.volia.net>
2018-06-12 16:53:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+“Physics isn't the most important thing. Love is.” 
+
+― Richard Feynman
---
+++ run.py
@@ -0,0 +1,21 @@
+import view as v
+visual = v.ViewData()
+
+""" Other tests all """
+# from scipy.io import loadmat
+# import matplotlib.pyplot as plt
+#
+#
+# sawdata = loadmat('saw_data.mat')
+# dp = sawdata['saw_data'][0, 0]['TIPROFILE'][0,0]['TIPRE'][0, 25]
+#
+# fig, axes = plt.subplots()
+# fig.set_size_inches(15, 8)
+#
+# axes.plot(
+#     range(0, len(dp)),
+#     dp
+# )
+#
+# axes.grid()
+# plt.show()
